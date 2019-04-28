d03b203f2849a5feb3d145a7fd6b6c77b667c008
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-04-28 21:51:31 +0900

    필요없는 내용을 제거하기 위해 elements 파일을 삭제한다

--- src/main/java/me/shinhong/sohwakhaeng/controller/MyController.java
+++ src/main/java/me/shinhong/sohwakhaeng/controller/MyController.java
@@ -1,6 +1,7 @@
 package me.shinhong.sohwakhaeng.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,6 @@ public class MyController {
     public String main(){
         return "index";
     }
+
 
 }
