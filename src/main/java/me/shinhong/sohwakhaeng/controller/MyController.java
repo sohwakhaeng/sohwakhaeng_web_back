5bbe5da2d92eb5b449ba4cbaf810b59fb2a9eb74
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-04-29 00:53:27 +0900

    userService를 사용하기 위해 Autowired를 추가한다

--- src/main/java/me/shinhong/sohwakhaeng/controller/MyController.java
+++ src/main/java/me/shinhong/sohwakhaeng/controller/MyController.java
@@ -1,11 +1,20 @@
 package me.shinhong.sohwakhaeng.controller;
 
+import me.shinhong.sohwakhaeng.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
+
+    @Autowired
+    public final UserService userService;
+
+    public MyController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping("/")
     public String main(){
